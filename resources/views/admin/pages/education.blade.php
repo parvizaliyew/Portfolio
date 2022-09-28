b54a838e3d540852a8d4b5d5a33b2875f54c1adc
parvizaliyew <eliyevperviz466@gmail.com>
2022-09-28 00:56:54 -0700

    I split the admin template

---
+++ resources/views/admin/pages/education.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.master')
+
+@section('title')
+    Admin Panel
+@endsection
+
+@section('content')
+    <div class="page-header">
+              <h3 class="page-title"> Form elements </h3>
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Forms</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Form elements</li>
+                </ol>
+              </nav>
+      </div>
+@endsection
--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -1,1 +1,11 @@
-salam burasi admin index seyfesidir
+@extends('admin.layouts.master')
+
+@section('title')
+    Admin Panel
+@endsection
+
+@section('content')
+    <div class="page-header">
+              <h1 class="page-title"> XOŞ GƏLDİNİZ  </h1>
+      </div>
+@endsection
